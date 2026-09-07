76093afd3088c6835f065ffb6d6627d4f22f9d46
rifat1025 <rifatsarkercse@gmail.com>
2026-09-07 12:07:02 +0600

    database configure

--- database.py
+++ database.py
@@ -1,22 +1,27 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL environment variable is not set.")
+DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
+print(DATABASE_URL)  # Test
 
 engine = create_engine(DATABASE_URL)
-print(engine)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
-
